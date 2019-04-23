2df370783663bc53d6722573b7fe2a29535d86c9
adho <adhoadhigal>
2019-04-23 09:31:52 +0300

    install flask-moment

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+class Config:
+	SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+	SECRET_KEY = os.environ.get("SECRET_KEY")
+	UPLOADED_PHOTOS_DEST = 'app/static/photos'
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+	SUBJECT_PREFIX = 'PITCH'
+	SENDER_EMAIL = 'adhoadhigal@gmail.com'
+	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adho:1234@localhost/pitch'
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adho:1234@localhost/pitch'
+
+class ProdConfig(Config):
+	pass
+	# SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adho:1234@localhost/pitch'
+
+	DEBUG = True
+
+
+
+config_options = {
+	'development': DevConfig,
+	'production':ProdConfig
+}	
---
+++ start.sh
@@ -0,0 +1,6 @@
+export PITCH_API_KEY="847d5d2867728336624023baf4ee5f4a"
+export SECRET_KEY="chukula"
+export MAIL_USERNAME=adhoadhigal@gmail.com
+export MAIL_PASSWORD=chukula13
+
+python3.6 manage.py server
