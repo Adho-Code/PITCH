411bb68da245705a0e626eec8a3103c7e1ab4c96
adho <adhoadhigal>
2019-04-23 10:08:05 +0300

    added category pitch

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField,SelectField
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
 from wtforms import ValidationError
@@ -11,6 +11,8 @@ class UpdateProfile(FlaskForm):
 
 
 class PitchForm(FlaskForm):
+    title=StringField("Welcome to Pitch Ideas")
+    Pitch_category = SelectField('Pitch Category',choices=[('Technology-Pitch','Technology Pitch'),('Business-Pitch','Business Pitch'),('Interview-Pitch','Interview Pitch'),('Pickup-Line','Pickup-Line Pitch'),('Promotion-Pitch','Promotion Pitch')],validators=[Required()])
     content = TextAreaField('YOUR PITCH')
     submit = SubmitField('SUBMIT')
 
